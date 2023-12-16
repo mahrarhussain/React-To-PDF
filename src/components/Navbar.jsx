5e9c1c0f2735113187af2dbc411f18a4b803577f
mahrarhussain07 <mahrarhussain9@gmail.com>
2023-12-16 19:13:52 +0500

    worked on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import navbarIcon from "../assets/icons/Logo-Icon.png"
 
 const Navbar = ({ onIconClick }) => {
   return (
     <nav className="navbar">
       <div className="navbar-icon" onClick={onIconClick}>
-        {/* Replace the following with your desired icon */}
-        <span>📄</span>
+        <img src={navbarIcon} alt="navbar"  />
       </div>
-      <div className="navbar-title">Art By Ahrar</div>
+      {/* <div className="navbar-title">BinaryArt By Ahrar</div> */}
     </nav>
   );
 };
